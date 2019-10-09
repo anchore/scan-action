9cc72e31c8483ea07fb291eee92978a216824a7c
Brady Todhunter <bradyt@anchore.com>
2019-10-09 15:13:00 -0700

    fix tests to run on local workstation
    Signed-off-by: Brady Todhunter <bradyt@anchore.com>

--- tests/index.test.js
+++ tests/index.test.js
@@ -5,6 +5,6 @@ const path = require('path');
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
     process.env['IMAGE_REFERENCE'] = 'docker.io/alpine:latest';
-    const ip = path.join(__dirname, 'index.js');
+    const ip = path.join(__dirname, '../index.js');
     console.log(cp.execSync(`node ${ip}`).toString());
 });
