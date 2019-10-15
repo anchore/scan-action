868b469aff4065b4ae1bf0a6016a74f4742c759a
Brady Todhunter <bradyt@anchore.com>
2019-10-15 16:54:46 -0700

    clean up debugging artifacts
    Signed-off-by: Brady Todhunter <bradyt@anchore.com>

--- index.js
+++ index.js
@@ -2,38 +2,22 @@ const core = require('@actions/core');
 const execSync = require('child_process').execSync;
 const fs = require('fs');
 
-// most @actions toolkit packages have async methods
 async function run() {
     try {
         core.debug((new Date()).toTimeString());
 
-        let image_reference = core.getInput('image_reference');
-        let dockerfile_path = core.getInput('dockerfile_path');
-        let scan_scriptname = core.getInput('scan_scriptname');
-        let inline_scan_image = core.getInput('inline_scan_image');
+        const image_reference = core.getInput('image_reference');
+        const dockerfile_path = core.getInput('dockerfile_path');
         let debug = core.getInput('debug');
         let fail_build = core.getInput('fail_build')
-        let policy_bundle_path = `${__dirname}/lib/critical_security_policy.json`
-        let policy_bundle_name = "critical_security_policy"
+        const policy_bundle_path = `${__dirname}/lib/critical_security_policy.json`
+        const policy_bundle_name = "critical_security_policy"
 
-        // overrides just for testing locally
-        //image_reference = "docker.io/alpine:3.8"
-        //image_reference = "mylocalimage:latest"
-        //image_reference = "docker.io/dnurmi/testrepo:node_critical_pass"
-        //dockerfile_path = "/tmp/Dockerfile"
-        //scan_scriptname = "inline_scan-v0.5.0"
-        //debug = "true"
-        //fail_build = "true"
+        const scan_scriptname = "inline_scan-v0.5.0"
+        const inline_scan_image = "docker.io/dnurmi/testrepo:inline-scan-slim-v0.5.1-dev"
 
         if (!image_reference) {
             throw new Error("Must specify a container image to analyze using 'image_reference' input")
-        }
-        if (!scan_scriptname) {
-            scan_scriptname = "inline_scan-v0.5.0"
-        }
-        if (!inline_scan_image) {
-            //inline_scan_image = "docker.io/dnurmi/testrepo:inline-scan-slim-v0.5.0"
-            inline_scan_image = "docker.io/dnurmi/testrepo:inline-scan-slim-v0.5.1-dev"	    
         }
         if (!debug) {
             debug = "false"
@@ -46,26 +30,24 @@ async function run() {
             fail_build = "true"
         }
 
-        console.log('Image: ', image_reference);
-        console.log('Dockerfile path: ', dockerfile_path);
-        console.log('Scriptname: ', scan_scriptname);
-        console.log('Inline Scan Image: ', inline_scan_image);
-        console.log('Debug Output: ', debug);
-        console.log('Fail Build: ', fail_build);
+        core.info('Image: ' +image_reference);
+        core.info('Dockerfile path: ' +dockerfile_path);
+        core.info('Inline Scan Image: ' +inline_scan_image);
+        core.info('Debug Output: ' +debug);
+        core.info('Fail Build: ' +fail_build);
 
         let cmd = `${__dirname}/lib/run_scan ${__dirname}/lib ${scan_scriptname} ${inline_scan_image} ${image_reference} ${debug} ${policy_bundle_path} ${policy_bundle_name}`;
         if (dockerfile_path) {
             cmd = `${cmd} ${dockerfile_path}`
         }
-        console.log('\nAnalyzing image: ' +image_reference)
+        core.info('\nAnalyzing image: ' +image_reference)
         execSync(cmd, {stdio: 'inherit'});
 
         let rawdata = fs.readFileSync('./anchore-reports/policy_evaluation.json');
         let policyEval = JSON.parse(rawdata);
-        let keys = Object.keys(policyEval[0]);
-        let policyStatus = policyEval[0][keys[0]][Object.keys(policyEval[0][keys[0]])[0]][0].status;
-
-        core.debug((new Date()).toTimeString());
+        let imageId = Object.keys(policyEval[0]);
+        let imageTag = Object.keys(policyEval[0][imageId[0]]);
+        let policyStatus = policyEval[0][imageId[0]][imageTag][0]['status']
 
         core.setOutput('time', new Date().toTimeString());
         core.setOutput('billofmaterials', './anchore-reports/content-os.json');
