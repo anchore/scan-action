4c346bc159ee493f642dd5873a9a9c2a957f6d69
Brady Todhunter <bradyt@anchore.com>
2019-10-15 16:54:05 -0700

    fix tests to setup default input values
    Signed-off-by: Brady Todhunter <bradyt@anchore.com>

--- tests/index.test.js
+++ tests/index.test.js
@@ -4,7 +4,9 @@ const path = require('path');
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-    process.env['IMAGE_REFERENCE'] = 'docker.io/alpine:latest';
+    process.env['INPUT_IMAGE_REFERENCE'] = 'alpine:latest';
+    process.env['INPUT_DEBUG'] = 'true';
+    process.env['INPUT_FAIL_BUILD'] = 'true';
     const ip = path.join(__dirname, '../index.js');
-    console.log(cp.execSync(`node ${ip}`).toString());
+    console.log(cp.execSync(`node ${ip}`, {env: process.env}, {stdio: 'inherit'}).toString());
 });
