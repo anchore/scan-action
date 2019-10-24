6a3678136f0b9f8c1d43b5b16a2bbea6b03ca61b
Steve Winton <stevewinton@gmail.com>
2019-10-24 14:29:47 -0500

    Reboot tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,12 +1,3 @@
-const process = require('process');
-const cp = require('child_process');
-const path = require('path');
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-    process.env['INPUT_IMAGE_REFERENCE'] = 'alpine:latest';
-    process.env['INPUT_DEBUG'] = 'true';
-    process.env['INPUT_FAIL_BUILD'] = 'true';
-    const ip = path.join(__dirname, '../index.js');
-    console.log(cp.execSync(`node ${ip}`, {env: process.env}, {stdio: 'inherit'}).toString());
+it('runs tests', () => {
+    expect(1 + 1).toBe(2);
 });
