284a2fad140c31e835b542fbba5be70263cfd0b6
Steve Winton <stevewinton@gmail.com>
2019-10-24 14:34:04 -0500

    Stub out tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,3 +1,17 @@
-it('runs tests', () => {
-    expect(1 + 1).toBe(2);
+jest.mock('@actions/core');
+jest.mock('child_process');
+jest.mock('fs');
+
+const core = require('@actions/core');
+const execSync = require('child_process').execSync;
+const fs = require('fs');
+
+describe('anchore-scan-action', () => {
+    beforeEach(() => {
+
+    });
+
+    it('runs tests', () => {
+        expect(1 + 1).toBe(2);
+    });
 });
