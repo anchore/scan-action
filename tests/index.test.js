f17bc57a5440258bdb84624bec20593bee7ac9f5
Brady Todhunter <bradyt@anchore.com>
2019-11-25 18:39:57 -0800

    split tests into unit & functional
    Signed-off-by: Brady Todhunter <bradyt@anchore.com>

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,6 @@
 jest.mock('@actions/core');
 jest.mock('@actions/exec');
+jest.mock('@actions/tool-cache');
 
 const _ = require('lodash');
 const core = require('@actions/core');
@@ -11,9 +12,47 @@ const main = require('..');
 const policyEvaluationFixture = require('./fixtures/policy_evaluation.fixture');
 const contentMergeFixture = require('./fixtures/content-merge.fixture');
 
-describe('anchore-scan-action', () => {
+describe('unit-tests', () => {
+    it('tests merge of outputs into single bill of materials with os-only packages', async () => {
+        let merged = main.mergeResults([contentMergeFixture["content-os.json"]]);
+        //console.log("os-only output: " +JSON.stringify(merged));
+        expect(merged.length).toBeGreaterThan(0);
+
+    });
+
+    it('tests merge of outputs into single bill of materials with all packages', async () => {
+        let merged = main.mergeResults([contentMergeFixture["content-os.json"], contentMergeFixture["content-npm.json"], contentMergeFixture["content-gem.json"], contentMergeFixture["content-java.json"], contentMergeFixture["content-python.json"]]);
+        //console.log("merged output: " +JSON.stringify(merged));
+        expect(merged.length).toBeGreaterThan(0);
+    });
+
+    it('tests finding content files in dir', async () => {
+        let testPath = path.join(__dirname, "fixtures");
+        fs.readdirSync = jest.fn(() => {
+            return Object.keys(contentMergeFixture);
+        });
+
+        let contentFiles = main.findContent(testPath);
+        expect(contentFiles.length).toEqual(5);
+    });
+
+    it('tests loading content in list', async () => {
+        fs.readFileSync = jest.fn((i) => {
+            return JSON.stringify(contentMergeFixture[i]);
+        });
+
+        let contentFiles = main.loadContent(Object.keys(contentMergeFixture));
+        expect(contentFiles.length).toEqual(5);
+    });
+});
+
+describe('functional-tests', () => {
     beforeEach(() => {
         exec.exec = jest.fn();
+        fs.readdirSync = jest.fn(() => {
+            return Object.keys(contentMergeFixture);
+        });
+        fs.writeFileSync = jest.fn();
 
         core.getInput = jest
             .fn()
@@ -48,37 +87,5 @@ describe('anchore-scan-action', () => {
         await main.run();
 
         expect(core.setFailed).toHaveBeenCalled();
-    });
-
-    it('tests merge of outputs into single bill of materials with os-only packages', async () => {
-        let merged = main.mergeResults([contentMergeFixture["content-os.json"]]);
-        //console.log("os-only output: " +JSON.stringify(merged));
-        expect(merged.length).toBeGreaterThan(0);
-
-    });
-
-    it('tests merge of outputs into single bill of materials with all packages', async () => {
-        let merged = main.mergeResults([contentMergeFixture["content-os.json"], contentMergeFixture["content-npm.json"], contentMergeFixture["content-gem.json"], contentMergeFixture["content-java.json"], contentMergeFixture["content-python.json"]]);
-        //console.log("merged output: " +JSON.stringify(merged));
-        expect(merged.length).toBeGreaterThan(0);
-    });
-
-    it('tests finding content files in dir', async () => {
-        let testPath = path.join(__dirname, "fixtures");
-        fs.readdirSync = jest.fn(() => {
-            return Object.keys(contentMergeFixture);
-        });
-
-        let contentFiles = main.findContent(testPath);
-        expect(contentFiles.length).toEqual(5);
-    });
-
-    it('tests loading content in list', async () => {
-        fs.readFileSync = jest.fn((i) => {
-            return JSON.stringify(contentMergeFixture[i]);
-        });
-
-        let contentFiles = main.loadContent(Object.keys(contentMergeFixture));
-        expect(contentFiles.length).toEqual(5);
     });
 });
