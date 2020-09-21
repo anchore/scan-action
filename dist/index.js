58247b286258c0e8eaaeba6dbff27de50fcd06e4
Alfredo Deza <adeza@anchore.com>
2020-09-21 12:15:37 -0400

    pin grype version
    Signed-off-by: Alfredo Deza <adeza@anchore.com>

--- dist/index.js
+++ dist/index.js
@@ -14,7 +14,7 @@ const fs = __webpack_require__(747);
 const defaultAnchoreVersion = '0.8.0';
 
 const grypeBinary = 'grype'
-const grypeVersion = '0.1.0-beta.6'
+const grypeVersion = '0.1.0-beta.7'
 
 // sarif code
 function convert_severity_to_acs_level(input_severity, severity_cutoff_param) {
@@ -356,7 +356,8 @@ async function downloadGrype(version) {
     core.debug(`Installing ${version}`);
 
     // Download the installer, and run
-    const installPath = await cache.downloadTool(`https://raw.githubusercontent.com/anchore/grype/main/install.sh`);
+    
+    const installPath = await cache.downloadTool(`https://raw.githubusercontent.com/anchore/grype/v${grypeVersion}/install.sh`);
     // Make sure the tool's executable bit is set
     await exec(`chmod +x ${installPath}`);
 
