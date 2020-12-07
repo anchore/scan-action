e6a6161ee1b94aeb4a039ceb38b2653ed7f2e9e8
Alfredo Deza <adeza@anchore.com>
2020-12-07 16:49:53 -0500

    git: ignore test output
    Signed-off-by: Alfredo Deza <adeza@anchore.com>

---
+++ tests/functional/test_images.py
@@ -0,0 +1,20 @@
+import os
+import pytest
+
+@pytest.fixture(scope="module")
+def image_output():
+    dirname = os.path.dirname(os.path.abspath(__file__))
+    output_file = os.path.join(dirname, 'output/image.output')
+    with open(output_file) as _f:
+        return _f.read()
+
+
+class TestSmoke:
+
+    # basic validation
+    def test_zero_exit_status(self, image_output):
+        lines = image_output.split()
+        assert lines[-1] == '0'
+
+    def test_found_vulnerabilities(self, image_output):
+        assert "Failed minimum severity level. Found vulnerabilities with level medium or higher" in image_output
