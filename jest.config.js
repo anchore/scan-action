1ff36c8d0fc3251f6bc5630e04e81db80d5f96f9
Keith Zantow <kzantow@gmail.com>
2021-08-24 16:20:01 -0400

    hopefully more useful jest summary report
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   setupFiles: ["<rootDir>/.jest/setEnvVars.js"],
   verbose: true,
   testPathIgnorePatterns: ["action.test.js"],
+  reporters: [["jest-summary-reporter", { failuresOnly: false }]],
 };
