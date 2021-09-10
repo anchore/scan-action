9edeb10d2e4b4e9763a17e5f786b18d2c9865426
Keith Zantow <kzantow@gmail.com>
2021-09-10 13:12:22 -0400

    Stop dumping Grype to logs; update snapshots
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,4 @@ module.exports = {
   setupFiles: ["<rootDir>/.jest/setEnvVars.js"],
   verbose: true,
   testPathIgnorePatterns: ["action.test.js"],
-  reporters: [["jest-summary-reporter", { failuresOnly: false }]],
 };
