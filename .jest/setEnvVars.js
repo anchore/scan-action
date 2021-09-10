606c2ef69234b3494b855df0dd16cd4ff1bab319
Keith Zantow <kzantow@gmail.com>
2021-09-10 17:03:27 -0400

    download pinned grype db for tests
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- .jest/setEnvVars.js
+++ .jest/setEnvVars.js
@@ -1,4 +1,2 @@
 process.env["RUNNER_TOOL_CACHE"] = "/tmp/actions/cache";
 process.env["RUNNER_TEMP"] = "/tmp/actions/temp";
-process.env["GRYPE_DB_AUTO_UPDATE"] = "false";
-process.env["GRYPE_DB_CACHE_DIR"] = "./grype-db";
