91e56704f29176520ca29473cfef2fe9068a61be
Andreas Bucksteeg <tricky42@users.noreply.github.com>
2022-01-13 16:58:00 +0100

    chore: Update Grype to 0.31.1
    Signed-off-by: Andreas Bucksteeg <tricky42@users.noreply.github.com>

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const stream = require("stream");
 
 const grypeBinary = "grype";
-const grypeVersion = "0.27.3";
+const grypeVersion = "0.31.1";
 
 // sarif code
 function convert_severity_to_acs_level(input_severity, severity_cutoff_param) {
